a85c1ba23d0add9d1e28950cac02ad63815cf337
Sergey Babichev <s.babichevv91p@gmail.com>
2022-09-02 16:23:18 +0300

    add websocket jwt current user

--- permissions.py
+++ permissions.py
@@ -3,10 +3,11 @@ from dataclasses import dataclass
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession, AsyncResult
 
-from customers.auth.interfaces.token_service_interface import TokenServiceInterface
+from customers.auth.interfaces.token_service_interface import \
+    TokenServiceInterface
 from settings import get_settings
 from database import get_db_session
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Header
 from sqlalchemy import select
 
 from customers.auth.models import Customers
@@ -30,10 +31,7 @@ class CustomerPermission:
     token_service: TokenServiceInterface
     configs = get_settings()
 
-    async def get_current_user(
-            self, token: str = Depends(OAUTH_TOKEN),
-            db: AsyncSession = Depends(get_db_session)
-    ):
+    async def receive_user(self, token: str, db: AsyncSession):
         payload: dict = await self.token_service.decode_token(token=token)
         if not (username := payload.get('sub')):
             raise PermissionException().username_not_found
@@ -43,3 +41,15 @@ class CustomerPermission:
         if not (user := result.scalars().first()):
             raise PermissionException().user_not_found
         return user
+
+    async def get_current_user(
+            self, token: str = Depends(OAUTH_TOKEN),
+            db: AsyncSession = Depends(get_db_session)
+    ):
+        return await self.receive_user(token=token, db=db)
+
+    async def jwt_websocket_current_user(
+            self, token: str | None = Header(default=None),
+            db: AsyncSession = Depends(get_db_session)
+    ):
+        return await self.receive_user(token=token, db=db)
